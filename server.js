751d0a43e9d8a0636763b023b1b1f44ee6bd7c1e
Robert Kojima <kojima.robert@gmail.com>
2017-04-06 14:41:58 -0700

    added morgan

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const config = require('./config');
+const morgan = require('morgan');
 
 const app = express();
 
+/* app.use(function(req, res, next) {
+    console.log(req.path);
+    console.log(req.method);
+    next();
+}); */
+
+app.use(morgan('dev'));
 // Exposing static files (files that client should see) 
 app.use(express.static('public'));
 
