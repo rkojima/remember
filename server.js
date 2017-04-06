4da81decee8498cad5f1e62f575cd88797e8d032
Robert Kojima <kojima.robert@gmail.com>
2017-04-06 14:26:41 -0700

    made form method , post no longer needs .html at end

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Exposing static files (files that client should see) 
 app.use(express.static('public'));
 
+app.post('/signup', function(req, res){
+    res.send('ok');
+});
 // No need for hostname yet
 app.listen(config.PORT, function() {
     console.log(`Listening on ${config.PORT}`);
