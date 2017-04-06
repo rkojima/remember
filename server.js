ab0e2c3574a1d460a4bdf453bfd00232cac269bc
Robert Kojima <kojima.robert@gmail.com>
2017-04-06 14:13:04 -0700

    able to see signup.html in public folder

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const config = require('./config');
 
 const app = express();
 
+// Exposing static files (files that client should see) 
+app.use(express.static('public'));
+
 // No need for hostname yet
 app.listen(config.PORT, function() {
     console.log(`Listening on ${config.PORT}`);
