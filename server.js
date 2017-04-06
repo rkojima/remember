5cf7622a138544902bbee9f6b19c3848679ab479
Robert Kojima <kojima.robert@gmail.com>
2017-04-06 14:56:02 -0700

    added bodyParser so that it parses data coming in

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const config = require('./config');
 const morgan = require('morgan');
 
 const app = express();
+const formParser = bodyParser.urlencoded();
 
 /* app.use(function(req, res, next) {
     console.log(req.path);
@@ -10,12 +12,16 @@ const app = express();
     next();
 }); */
 
-app.use(morgan('dev'));
+app.use(morgan('dev')); // Makes it easier to develop
+// Morgan is first so that we see all actions done
+
+
+
 // Exposing static files (files that client should see) 
 app.use(express.static('public'));
 
-app.post('/signup', function(req, res){
-    res.send('ok');
+app.post('/signup', formParser, function(req, res){
+    res.json(req.body);
 });
 // No need for hostname yet
 app.listen(config.PORT, function() {
