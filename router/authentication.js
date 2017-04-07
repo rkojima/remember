8d90bdbf26ca0c2983680fa458c95de4d0a12a40
Robert Kojima <kojima.robert@gmail.com>
2017-04-07 14:52:35 -0700

    added way to create individual users for users signing up

--- router/authentication.js
+++ router/authentication.js
@@ -2,13 +2,23 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const {User} = require('../models/user');
 
 const router = express.Router();
 const formParser = bodyParser.urlencoded();
 
 router.post('/signup', formParser, function(req, res) {
-    // body...
-    res.json(req.body);
+    User.create({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.psw, // TODO change to hash
+    })
+    .then(function(user) {
+        res.json(`User ${user.id} created.`);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
 });
 
 module.exports = {router};
