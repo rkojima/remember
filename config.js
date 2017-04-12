783c22f2fb934d0e4943ec28adf99dcbc0c8e94a
Robert Kojima <kojima.robert@gmail.com>
2017-04-12 14:45:26 -0700

    in case I need a test database url

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        'mongodb://localhost/rememorari';
+exports.TEST_DATABASE_URL = (
+    process.env.TEST_DATABASE_URL ||
+    'mongodb://localhost/test-rememorari');
 exports.PORT = process.env.PORT || 8080;
 
 // To configure the database URL, so that it could get, post, put, delete, etc.?
