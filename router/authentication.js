ca4d0a5a7c8cace9dd830e89157cb3ed8d798485
Robert Kojima <kojima.robert@gmail.com>
2017-04-13 14:08:35 -0700

    added auth for login, /login should work

--- router/authentication.js
+++ router/authentication.js
@@ -34,16 +34,20 @@ passport.use(basicStrategy);
 router.use(passport.initialize());
 router.use(passport.session());
 
-passport.use(new LocalStrategy(
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'psw'
+},
   function(username, password, done) {
-    console.log('Here');
+    console.log(`Username: ${username}`);
+        console.log(`Password: ${password}`);
+        console.log('Here');
     User.findOne({ username: username }, function(err, user) {
-        console.log('There');
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-      if (!user.validPassword(password)) {
+      if (!user.validatePassword(password)) {
         return done(null, false, { message: 'Incorrect password.' });
       }
       return done(null, user);
@@ -51,14 +55,15 @@ passport.use(new LocalStrategy(
   }
 ));
 
-
-
 passport.serializeUser(function(user, done) {
-  done(null, user);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+    console.log(User);
+  User.findById(user.id, function(err, user) {
+    done(err, user);
+  });
 });
 
 router.get('/', function(req, res) {
@@ -78,7 +83,7 @@ router.post('/signup', formParser, function(req, res) {
     })
     .then(function(user) {
         res.json(`User ${user.id} created.`);
-    })
+    }) 
     .catch(function(err) {
         if (err.code === 11000) {
             res.status(400).send(`There's already a user with that name.`);
@@ -91,9 +96,12 @@ router.post('/signup', formParser, function(req, res) {
 });
 
 // A route when logging in
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.send(req.user);
-});
+const loginMiddleware = passport.authenticate('local', {
+        successRedirect: '/', // TODO change this to dashboard
+        failureRedirect: '/login'
+    });
+// Made sure that info from form was coming in through post
+router.post('/login', formParser, loginMiddleware);
 
 // Menu, where they see their books and notes
 router.get('/dashboard', passport.authenticate('local', {session: false}), function(req, res) {
