8b4639ecf7455bb67fd1b457dbd6a9994964188f
Robert Kojima <kojima.robert@gmail.com>
2017-04-14 14:28:24 -0700

    created post method for adding books

--- models/book.js
+++ models/book.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const bookSchema = mongoose.Schema({
-    bookTitle: {
+    title: {
         type: String,
     },
     pages: {
--- router/createBook.js
+++ router/createBook.js
@@ -9,4 +9,14 @@ router.get('/create-book', function(req, res) {
     res.render('createBook', {title: "Create a book for your library!"});
 });
 
+router.post('/create-book', formParser, function(req, res) {
+    Book.create({
+        title: req.body.book,
+        pages: req.body.pages
+    })
+    .then(function(book) {
+        res.send(`The book, "${book.title}," was created.`);
+    });
+});
+
 module.exports = {router};
