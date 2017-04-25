8191ee0d9c1eeadaee56114d6690ff1c13ba0893
Robert Kojima <kojima.robert@gmail.com>
2017-04-25 10:43:03 -0700

    added safeguards on whether people own a book or not, also working on showing the books in a library

--- router/book.js
+++ router/book.js
@@ -30,6 +30,11 @@ router.post('/create-book', authenticatedOnly, formParser, function(req, res) {
 });
 
 router.post('/add-book', authenticatedOnly, formParser, function(req,res) {
+    // check if book hasn't been already added, look at userOwns = req.isAuthenticated
+    if (req.user.ownBook(req.body.book)) {
+        res.redirect('/book/' + req.body.book);
+    } 
+    else {
     const book = req.body.book;
     console.log(req.body.book);
     User.findOneAndUpdate(
@@ -39,6 +44,7 @@ router.post('/add-book', authenticatedOnly, formParser, function(req,res) {
     .then(function(user) {
         res.redirect('/book/' + book);
     });
+}
 });
 
 router.get('/book/:id', function(req, res) {
@@ -46,7 +52,7 @@ router.get('/book/:id', function(req, res) {
     Book.findById(req.params.id)
     .then(function(book) {
         const userOwns = req.isAuthenticated() ? 
-        req.user.library.map(bookId => bookId.toString()).includes(req.params.id) : false;
+        req.user.ownBook(req.params.id) : false;
         const showAddButton = req.user && !userOwns;
         res.render("book", {title: book.title, book: book, owned: userOwns, user: req.user, addButton: showAddButton});
     })
@@ -54,6 +60,25 @@ router.get('/book/:id', function(req, res) {
         console.log(err);
         res.sendStatus(404);
     });
+});
+
+router.get('/dashboard', authenticatedOnly, function(req, res) {
+    // res.json({library: req.user.library});
+    let libraryArray = [];
+    req.user.library.forEach(function(book) {
+        // mongoDB right here
+        Book.findById(book)
+        .then(function(book) {
+            libraryArray.push([
+              book.title, book.pages  
+            ]);
+            console.log(libraryArray);
+        });
+
+    });
+    console.log(libraryArray);
+    res.send(libraryArray);
+    // Having trouble with asynchronous operations, need to somehow do res.send after finding the books in the collection
 });
 
 module.exports = {router};
