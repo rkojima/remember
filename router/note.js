b3237a3f0d92264a4c504b2c3ac283571a424b45
Robert Kojima <kojima.robert@gmail.com>
2017-05-02 13:27:12 -0700

    router now renders note hbs page

--- router/note.js
+++ router/note.js
@@ -12,8 +12,11 @@ const {authenticatedOnly} = require('./authentication');
 const router = express.Router();
 const formParser = bodyParser.urlencoded();
 
-router.get('/notes/:id', authenticatedOnly, function(req, res) {
-    res.send("Hello World");
+router.get('/notes/:id', authenticatedOnly, formParser, function(req, res) {
+    Book.findById(req.params.id)
+    .then(function(book) {
+        res.render("note", populateVariables(req, {bookName: book.title}));
+    });
 });
 
 module.exports = {router};
