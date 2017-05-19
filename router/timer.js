a43c85932405add8b0d908e4fdf4f1e8dd1c26d7
Robert Kojima <kojima.robert@gmail.com>
2017-05-19 14:21:53 -0700

    deleted comments, loaders that were placed in util.js. added function to hbs

--- router/timer.js
+++ router/timer.js
@@ -3,54 +3,23 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const moment = require('moment');
 
-const {populateVariables} = require('./util');
+const {populateVariables, userLibraryLoader, noteLoader, bookLoader, timerLoader} = require('./util');
 const {Book} = require('../models/book');
 const {User} = require('../models/user');
-const {Note, bookLoader, noteLoader} = require('../models/note');
+const {Note} = require('../models/note');
 const {Timer} = require('../models/timer');
 const {authenticatedOnly} = require('./authentication');
 
 const router = express.Router();
 const formParser = bodyParser.urlencoded({extended: true});
 
-function timerLoader(req, res, next) {
-    if (!req.params.timerId) {
-        res.status(400).send("Please send valid timer ID");
-    }
-    else {
-        // Load book first
-        Timer.findById(req.params.timerId)
-        .then(function(timer) {
-            req.timer = timer;
-            next();
-        })
-        .catch(function(err) {
-            res.status(400).send(err);
-        });
-    }
-}
-
-router.get('/timer', authenticatedOnly, formParser, function(req, res) {
+router.get('/timer', authenticatedOnly, formParser, userLibraryLoader, function(req, res) {
         //authenticatedOnly should use passport and get me the user
         //then I should be able to get user's library
         //then populate it with books from library
         //then get book titles from the book
-
-        // Book.find(req.user.library)
-        // .then(book => {
-        //     console.log("Test 1 " + book);
-        // });
-        // Need to somehow keep the book id to transfer it to the post request
-        Book.find(req.user.library)
-        .populate('myBook')
-        .then(function(book) {
-            res.render('timer', populateVariables(req, {books: book}));
-        });
-
-        // console.log("Test 3" + req.user);
-        // req.user.library.forEach(function(book) {
-        //     console.log(book.myBook);
-        // });
+        console.log(req.user.library);
+        res.render('timer', populateVariables(req, {books: req.user}));
 });
 
 router.post('/timer', authenticatedOnly, formParser, function(req, res) {
@@ -58,14 +27,14 @@ router.post('/timer', authenticatedOnly, formParser, function(req, res) {
     // if (req.body.minutes < 1 || req.body.minutes === null) {
 
     // }
-    console.log(req.user);
-    console.log(req.body);
-    Book.find({title: req.body.title})
-    .then(function (book) {
-        // body...
-        User.find()
-    });
-    // Timer.create({
+    // console.log(req.user);
+    // console.log(req.body);
+    // Book.find({title: req.body.title})
+    // .then(function (book) {
+    //     // body...
+    //     User.find()
+    // });
+    // // Timer.create({
     //     user: req.user,
     //     book: req.book,
     //     endTime: moment().add(req.body.minutes, "m"),
